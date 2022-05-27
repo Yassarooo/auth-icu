74f555429da032a195c1e5f5b9d1c2666178574b
Yassar Hammami <yassarhammami@gmail.com>
2022-05-28 00:08:35 +0300

    Update WebSecurityConfig.java

--- src/main/java/com/jazara/icu/auth/config/WebSecurityConfig.java
+++ src/main/java/com/jazara/icu/auth/config/WebSecurityConfig.java
@@ -55,7 +55,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity
                 .sessionManagement().sessionCreationPolicy( SessionCreationPolicy.STATELESS ).and()
                 .exceptionHandling().authenticationEntryPoint( jwtAuthenticationEntryPoint ).and()
-                .authorizeRequests().antMatchers("/","/v2/**","/swagger-ui.html","/mgmt/**","/actuator/**", "/registration/**", "/index.html", "/app.js", "/route.js", "/app/**","/api/**", "/favicon.ico", "/prometheus/**", "/actuator/**", "/metrics/**").permitAll()
+                .authorizeRequests().antMatchers("/","/v2/**","/swagger-ui/**","/swagger-ui.html","/mgmt/**","/actuator/**", "/registration/**", "/index.html", "/app.js", "/route.js", "/app/**","/api/**", "/favicon.ico", "/prometheus/**", "/actuator/**", "/metrics/**").permitAll()
                 .anyRequest().authenticated().and();
 
         httpSecurity.csrf().disable();
