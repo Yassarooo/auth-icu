61f57c37733e0cffc9795f132ad5237797b5ea05
Yassar Hammami <yassarhammami@gmail.com>
2022-05-27 23:54:52 +0300

    .

--- src/main/java/com/jazara/icu/auth/AuthApplication.java
+++ src/main/java/com/jazara/icu/auth/AuthApplication.java
@@ -14,11 +14,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableZuulProxy
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableDiscoveryClient
 @EnableEncryptableProperties
+@EnableSwagger2
 public class AuthApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
@@ -32,6 +35,12 @@ public class AuthApplication implements CommandLineRunner {
 
     @Autowired
     RoleService roleService;
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.jazara.icu.auth")).build();
+    }
 
     @Override
     public void run(String... strings) throws Exception {
