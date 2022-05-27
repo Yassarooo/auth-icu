c8ba48c6951fab511c3cbb3b5481078f14c0ab3d
Yassar Hammami <yassarhammami@gmail.com>
2022-05-28 01:02:44 +0300

    Update AuthApplication.java

--- src/main/java/com/jazara/icu/auth/AuthApplication.java
+++ src/main/java/com/jazara/icu/auth/AuthApplication.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
