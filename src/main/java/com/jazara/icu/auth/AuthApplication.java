c2a6288f0447637cb012d70253c13e81fbde4eda
Yassar Hammami <yassarhammami@gmail.com>
2022-05-28 04:21:35 +0300

    Update AuthApplication.java

--- src/main/java/com/jazara/icu/auth/AuthApplication.java
+++ src/main/java/com/jazara/icu/auth/AuthApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @EnableZuulProxy
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableDiscoveryClient
 @EnableEncryptableProperties
 public class AuthApplication implements CommandLineRunner {
