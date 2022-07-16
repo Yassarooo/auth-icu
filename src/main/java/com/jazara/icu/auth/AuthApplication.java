2c196e41a78eb307bd652591d9d3ba4742659079
Yassar Hammami <yassarhammami@gmail.com>
2022-07-16 04:33:37 +0300

    .

--- src/main/java/com/jazara/icu/auth/AuthApplication.java
+++ src/main/java/com/jazara/icu/auth/AuthApplication.java
@@ -5,7 +5,6 @@ import com.jazara.icu.auth.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
