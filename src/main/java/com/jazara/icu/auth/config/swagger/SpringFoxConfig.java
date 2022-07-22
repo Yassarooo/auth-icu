e6cf4b7f9894fd36d2df56dfef0df8c33d3d838e
Yassar Hammami <yassarhammami@gmail.com>
2022-07-22 05:09:11 +0300

    swagger with eureka

--- src/main/java/com/jazara/icu/auth/config/swagger/SpringFoxConfig.java
+++ src/main/java/com/jazara/icu/auth/config/swagger/SpringFoxConfig.java
@@ -1,20 +1,49 @@
-package com.jazara.icu.auth.config;
+package com.jazara.icu.auth.config.swagger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.client.RestTemplate;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.InMemorySwaggerResourcesProvider;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 @Configuration
 public class SpringFoxConfig {
+
+    @Autowired
+    private ServiceDefinitionsContext definitionContext;
+
+    @Bean
+    public RestTemplate configureTempalte() {
+        return new RestTemplate();
+    }
+
+    @Primary
+    @Bean
+    @Lazy
+    public SwaggerResourcesProvider swaggerResourcesProvider(InMemorySwaggerResourcesProvider defaultResourcesProvider, RestTemplate temp) {
+        return () -> {
+            List<SwaggerResource> resources = new ArrayList<>(defaultResourcesProvider.get());
+            resources.clear();
+            resources.addAll(definitionContext.getSwaggerDefinitions());
+            return resources;
+        };
+    }
+
 
     private ApiKey apiKey() {
         return new ApiKey("JWT", "Authorization", "header");
