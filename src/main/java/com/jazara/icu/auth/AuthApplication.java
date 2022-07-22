f07ea6527647f9be159c5467b824fd135a42d0fc
Yassar Hammami <yassarhammami@gmail.com>
2022-07-22 04:38:37 +0300

    swagger3

--- src/main/java/com/jazara/icu/auth/AuthApplication.java
+++ src/main/java/com/jazara/icu/auth/AuthApplication.java
@@ -5,19 +5,19 @@ import com.jazara.icu.auth.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableZuulProxy
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableDiscoveryClient
+@EnableSwagger2
 @EnableEncryptableProperties
 public class AuthApplication implements CommandLineRunner {
 
