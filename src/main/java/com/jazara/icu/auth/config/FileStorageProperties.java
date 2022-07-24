1cc5c55a02b7eca37cd17216c08b07e49060e6cf
Yassar Hammami <yassarhammami@gmail.com>
2022-07-24 23:54:44 +0300

    Update FileStorageProperties.java

--- src/main/java/com/jazara/icu/auth/config/FileStorageProperties.java
+++ src/main/java/com/jazara/icu/auth/config/FileStorageProperties.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "file")
 public class FileStorageProperties {
-    private String uploadDir;
+    private String uploadDir = "/app";
 
     public String getUploadDir() {
         return uploadDir;
